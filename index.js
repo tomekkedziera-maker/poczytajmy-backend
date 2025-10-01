8222d90fc439b331734915c1382ed967ed7e9f5f
Tomek Kedziera <twoj_email@example.com>
2025-10-01 15:18:17 +0200

    fix: usuń linie spoza JS na początku pliku; bump 1.14-object-priority

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-﻿# 1) Wejdź do katalogu backendu
-cd D:\poczytajmy-backend
-
-# 2) (opcjonalnie) utwórz branch
-git checkout -b fix/comprehend-priority
-
-# 3) Wklej nową zawartość index.js z CLIPBOARD (skopiuj cały plik, potem:)
-Set-Content -Path .\index.js -Value (Get-Clipboard) -Encoding UTF8
-
-# 4) Commit + push
-git add .\index.js
-git commit -m "comprehend: priorytet dopełnienia (Co/Czego/W co) w 1. os.; doprecyzowany prompt 3. os."
-git push origin HEAD
-
+﻿
